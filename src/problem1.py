9e6a3fd095631e0d16e73422ad3d9ac544e2d030
halenv <halenv@rose-hulman.edu>
2018-05-24 13:14:54 -0400

    p1 in progress

--- src/problem1.py
+++ src/problem1.py
@@ -2,9 +2,9 @@
 Final exam, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and Noelle Hale.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -19,7 +19,7 @@ def main():
     print()
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
-    # run_test_problem1a()
+    run_test_problem1a()
     # run_test_problem1b()
 
 
@@ -90,10 +90,16 @@ def problem1a(numbers):
       :type numbers:  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
+    num_first = numbers[0]
+    num_last = numbers[len(numbers) - 1]
+    num_mid = numbers[len(numbers) // 2]
+
+    sum_num = num_first + num_mid + num_last
+    return sum_num
 
 def run_test_problem1b():
     """ Tests the  problem1b   function. """
